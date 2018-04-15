3d745a82f1ed6ab7964cb24bbd13b9f88a1cafe9
eunmo <eunmo@cs.stanford.edu>
2018-04-15 19:24:15 +0900

    update cup graphic to match new data structure which contains all matches

--- src/components/Graphics/Cup/index.js
+++ src/components/Graphics/Cup/index.js
@@ -11,34 +11,45 @@ export default class Cup extends Component {
 
 	render() {
 		let cup = this.props.cup;
-		let sorted = this.getSorted();
-		let grid = this.getGrid(sorted);
+		let rounds = this.getRounds(cup);
 
-		let width = 350;
-		let height = 350;
-		
-		var circles = [];
-		let cx = width / 2;
-		let cy = height / 2;
+		if (rounds.length === 0)
+			return null;
 
-		var teams = [];
-		var i, j, round, level, team;
-		var x, y, r, theta, log2, dTheta;
-		var size, hsize, lsize, hlsize;
+		this.makeGrid(cup, rounds);
+		this.calculateBase(rounds);
+
+		console.log(cup);
+		console.log(rounds);
 
 		let filterId = 'greyscale' + Math.random();
 		let filterUrl = 'url(#' + filterId + ')';
 
 		var lastRound = [];
-		let lastRoundIndex = this.getLastRound(sorted);
+		var teams = [];
+		var circles = [];
+		var cupWinner = null;
 		var image;
+		
+		let width = 350;
+		let height = 350;
+		
+		let cx = width / 2;
+		let cy = height / 2;
+		
+		let wsize = 40;
+		let hwsize = wsize / 2;
 
-		for (i = 0; i < grid.length; i++) {
-			round = grid[i];
+		var i, j, round, level, team;
+		var x, y, r, theta, log2, dTheta;
+		var size, hsize, lsize, hlsize;
+
+		for (i = 0; i < rounds.length; i++) {
+			round = rounds[i];
 			level = round.level;
-			log2 = this.getlog2(level);
+			log2 = round.log2;
 			dTheta = 2 * Math.PI / level;
-				
+
 			r = 25 * log2 + 6;
 
 			size = 28 - log2 * 2;
@@ -47,16 +58,8 @@ export default class Cup extends Component {
 			lsize = size - 4;
 			hlsize = lsize / 2;
 			
-			if (i === lastRoundIndex) {
-				if (i === 0) {
-					lastRound.push(<circle key={0} cx={cx} cy={cy} r={r + 13} fill="#f0f0f0" />);
-				} else {
-					lastRound.push(<circle key={0} cx={cx} cy={cy} r={r} stroke="#f0f0f0" strokeWidth="25" fill="none" />);
-				}
-			}
-			
 			circles.push(<circle key={r} cx={cx} cy={cy} r={r + 13} stroke="lightgrey" strokeWidth="1" fill="none" />);
-
+		
 			if (i === 0) {
 				if (log2 > 1 || cup.winner === undefined) {
 					r -= 10;
@@ -65,38 +68,35 @@ export default class Cup extends Component {
 				}
 			}
 			
-			for (j = 0; j < round.teams.length; j++) {
-				team = round.teams[j];
+			for (j = 0; j < round.grid.length; j++) {
+				team = round.grid[j];
 
 				if ((team === undefined) ||
 						(i > 0 && j % 2 === 0) ||
 						(cup.winner && i + j === 0))
 					continue;
-
+				
 				theta = dTheta * (j - 1);
 				if (i === 0 && log2 !== 1) {
 					theta += dTheta;
 				}
+				
 				x = cx + r * Math.cos(theta) - hsize;
 				y = cy + r * Math.sin(theta) - hsize;
 
-				image = <image key={level + team} xlinkHref={UrlUtil.getEmblemUrl(team)} x={x} y={y} width={size} height={size} />;
+				image = <image key={log2 + team} xlinkHref={UrlUtil.getEmblemUrl(team)} x={x} y={y} width={size} height={size} />;
 				teams.push(this.getLink(image, team));
-
-				if (((i === 0 && cup.winner) || (i > 0 && i < grid.length - 1)) &&
-						grid[i + 1].teams[2 * j] === team) {
+				
+				if (((i === 0 && cup.winner) || (i > 0 && i < rounds.length - 1)) &&
+						rounds[i + 1].grid[2 * j] === team) {
 					theta += dTheta;
 					x = cx + r * Math.cos(theta) - hlsize;
 					y = cy + r * Math.sin(theta) - hlsize;
-					image = <image key={level + team + '2'} xlinkHref={UrlUtil.getEmblemUrl(team)} x={x} y={y} width={lsize} height={lsize} filter={filterUrl} />;
+					image = <image key={log2 + team + '2'} xlinkHref={UrlUtil.getEmblemUrl(team)} x={x} y={y} width={lsize} height={lsize} filter={filterUrl} />;
 					teams.push(this.getLink(image, team));
 				}
 			}
 		}
-
-		let wsize = 40;
-		let hwsize = wsize / 2;
-		var cupWinner = null;
 
 		if (cup.winner) {
 			image = <image xlinkHref={UrlUtil.getEmblemUrl(cup.winner)} x={cx - hwsize}  y={cy - hwsize} width={wsize} height={wsize} />;
@@ -122,117 +122,134 @@ export default class Cup extends Component {
 					</svg>
 				</div>
 			</div>
-		)
+		);
 	}
 
-	getlog2(level) {
-		for (var i = 1; i < 10; i++) {
-			if (Math.pow(2, i) === parseInt(level, 10)) {
-				return i;
+	calculateBase(rounds) {
+		var base = 1;
+		var log2 = 0;
+		var i, round;
+
+		for (i = 0; i < rounds.length; i++) {
+			round = rounds[i];
+			while (base << i < round.grid.length) {
+				base = base << 1;
+				log2++;
 			}
 		}
-	}
-
-	getSorted() {
-		let cup = this.props.cup;
-		let roundNumber = rounds.getRoundNumbers(cup.name);
-
-		var array = [];
-		cup.rounds.forEach(function (round) {
-			if (!round.name.match(/^Group/)) {
-				array.push(round);
-			}
-		});
-		array.sort(function(a, b) {
-			if (a.name === 'Final') {
-				return -1;
-			} else if (b.name === 'Final') {
-				return 1;
-			} else {
-				return parseInt(roundNumber[a.name], 10) - parseInt(roundNumber[b.name], 10);
-			}
-		});
-
-		return array;
+		
+		for (i = 0; i < rounds.length; i++) {
+			round = rounds[i];
+			round.level = base << i;
+			round.log2 = log2 + i;
+		}
 	}
 
 	arrayToMap(array) {
 		var map = {};
-		
-		array.forEach(function (elem, index) {
-			map[elem] = index;
-		});
+		var i, entry;
+
+		for (i = 0; i < array.length; i++) {
+			entry = array[i];
+
+			if (entry) {
+				map[entry] = i;
+			}
+		}
 
 		return map;
 	}
 
-	getGrid(sorted) {
-		let cup = this.props.cup;
-		let roundNumber = rounds.getRoundNumbers(cup.name);
-		var grid = [];
-		var round, prevLevel, prevMap;
-		var i, j;
-		var match, index;
+	makeGrid(cup, rounds) {
+		var i, round;
+		var j, teamL, teamR, index;
+		var k, offset;
+		var map, prevGrid, grid;
 
-		for (i = 0; i < sorted.length; i++) {
-			round = sorted[i];
+		round = rounds[0];
+		round.grid = round.teams;
 
-			let level = roundNumber[round.name];
-			let array = [];
+		if (cup.winner && round.grid[1] === cup.winner) {
+			round.grid.reverse();
+		}
 
-			if (round.name === 'Final') {
-				array[0] = round.matches[0][0];
-				array[1] = round.matches[0][1];
-				if (cup.winner === array[1]) {
-					array.reverse();
-				}
-				level = 2;
-			} else if (i === 0) {
-				for (j = 0; j < round.matches.length; j++) {
-					match = round.matches[j];
-					array.push(match[0], match[1]);
-				}
-			} else {
-				for (j = 0; j < round.matches.length; j++) {
-					match = round.matches[j];
+		for (i = 1; i < rounds.length; i++) {
+			round = rounds[i];
+			rounds[i - 1].map = this.arrayToMap(rounds[i - 1].grid);
+			grid = [];
 
-					if (prevMap[match[0]] !== undefined) {
-						index = prevMap[match[0]];
-						array[index * 2] = match[0];
-						array[index * 2 + 1] = match[1];
-					} else if (prevMap[match[1]] !== undefined) {
-						index = prevMap[match[1]];
-						array[index * 2] = match[1];
-						array[index * 2 + 1] = match[0];
+			for (j = 0; j < round.teams.length; j += 2) {
+				teamL = round.teams[j];
+				teamR = round.teams[j + 1];
+
+				k = i - 1;
+				while (k >= 0) {
+					map = rounds[k].map;
+					offset = 1 << (i - k);
+				
+					if (map[teamL] !== undefined) {
+						grid[map[teamL] * offset] = teamL;
+						grid[map[teamL] * offset + 1] = teamR;
+						break;
+					} else if (map[teamR] !== undefined) {
+						grid[map[teamR] * offset] = teamR;
+						grid[map[teamR] * offset + 1] = teamL;
+						break;
 					}
+
+					k--;
+				}
+
+				if (k === -1) {
+					index = Math.max(rounds[i - 1].grid.length * 2, grid.length);
+					grid[index] = teamL;
+					grid[index + 1] = teamR;
 				}
 			}
 
-			prevLevel = { level: level, teams: array };
-			grid.push(prevLevel);
-			
-			prevMap = this.arrayToMap(array);
+			round.grid = grid;
 		}
-
-		return grid;
 	}
 
-	getLastRound(sorted) {
+	addIfNew(team, map, array) {
+		if (map[team] === undefined) {
+			map[team] = array.length;
+			array.push(team);
+		}
+	}
 
-		var i, j;
-		var round, match;
+	getRounds(cup) {
+		var rounds = [];
+		var i, round;
+		var j, match;
+		var map, teams;
+		var count;
 
-		for (i = 0; i < sorted.length; i++) {
-			round = sorted[i];
+		for (i = 0; i < cup.rounds.length; i++) {
+			round = cup.rounds[i];
+			teams = [];
+			map = {};
+
+			if (round.name.includes('Group')) {
+				rounds = []; // removes rounds before group stage
+				continue;
+			}
+
 			for (j = 0; j < round.matches.length; j++) {
 				match = round.matches[j];
-				if (match[0] === this.props.team || match[1] === this.props.team) {
-					return i;
-				}
+				this.addIfNew(match.l, map, teams);
+				this.addIfNew(match.r, map, teams);
 			}
+
+			rounds.push({
+				raw: round,
+				teams: teams
+			});
 		}
 
-		return null;
+		rounds.reverse();
+
+		return rounds;
 	}
 
 	getLink(image, team) {
