b4caff67b64dcb46b283b8d9b6008384cdc6f0bd
uihyun <uihyunkei@gmail.com>
2018-05-07 04:15:06 +0900

    chagne host into heroku

--- cron/update.sh
+++ cron/update.sh
@@ -1,2 +1,2 @@
-curl "localhost:3050/api/match/fetch-season/2018"
-curl "localhost:3050/api/league/update-all/2018"
+curl "football-archive.herokuapp.com/api/match/fetch-season/2018"
+curl "football-archive.herokuapp.com/api/league/update-all/2018"
