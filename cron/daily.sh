6452d53bc1fbac208978b5a83e293f4debaa3ebf
eunmo <eunmo@cs.stanford.edu>
2018-05-22 21:00:24 +0900

    update daily cron script for kfa cup

--- cron/daily.sh
+++ cron/daily.sh
@@ -1,5 +1,6 @@
 curl "localhost:3050/api/season/update/2018"
 curl "localhost:3050/api/korea/league/update/2018/kleague"
 curl "localhost:3050/api/korea/league/update/2018/kleague2"
+curl "localhost:3050/api/korea/cup/update/2018"
 curl "localhost:3050/api/korea/assemble/2018"
 curl "localhost:3050/api/match/clear/recent/2018"
